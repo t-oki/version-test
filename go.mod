744632ff2a65a5c5c4430470703069ff23492b47
t-oki <blutac.nz@gmail.com>
2022-06-08 21:02:40 +0900

    version test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/t-oki/version-test
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+func main() {
+	buildInfo, ok := debug.ReadBuildInfo()
+	if ok {
+		fmt.Println("Failed to get version.")
+		return
+	}
+	fmt.Printf("Version: %s", buildInfo.Main.Version)
+}
